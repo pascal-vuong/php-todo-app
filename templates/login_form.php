988006fd64a1fd86c6e76d637f3d9086bc1187e7
Pascal Vuong <pascalvuongcoding@gmail.com>
2025-09-14 00:00:52 +0200

    Update login_form.php
    loaded style.css
    moved title, input fields, and button inside a div

--- templates/login_form.php
+++ templates/login_form.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
+    <title>Register</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    <h2>Login</h2>
+
+<div class="form-container">
+
+    <h2>Register</h2>
     
-    <form method="post" action="login.php">
+    <form method="post" action="register.php">
         Username: <input type="text" name="username" required><br><br>
         Password: <input type="password" name="password" required><br><br>
-        <button type="submit">Login</button>
+        <button type="submit">Register</button>
     </form>
 
+</div>
+
     <p><?php echo $message; ?></p>
+
 </body>
 
 </html>
